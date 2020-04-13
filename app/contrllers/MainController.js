b36eda8e36afe2c697589edc577510f5039df246
ravindukariyapperuma <ravindu.kariyapperuma@gmail.com>
2020-04-14 00:58:23 +0530

    'MainController'

---
+++ app/contrllers/MainController.js
@@ -0,0 +1,6 @@
+app.controller('MainController', ['$scope', $scope => {
+    $scope.list = ['task 1', 'task 2', 'task 3'];
+    $scope.addItem = () => {
+        $scope.list.push($scope.addToDo);
+    }
+}]);
